9b2515e74bdd7aaa191ec66b54dd2a6d31414bf4
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-12 22:01:41 +0530

    Find Maximum Of 3 Values Using Generics Class
    Find Maximum Of 3 Values Using Generics Class

--- MaxOf3ValuesClass.cs
+++ MaxOf3ValuesClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace FindMaxOf3ValuesClass
 {
@@ -15,18 +15,21 @@ namespace FindMaxOf3ValuesClass
 
         public static T findmax(T first, T second, T third)
         {
+            //If first-second>0 && first-third>0
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
                 first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
                 first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
             {
                 return first;
             }
+            //If second-first>0 && second-third>0
             if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
             {
                 return second;
             }
+            //If third-first>0 && third-second>0
             if (third.CompareTo(second) > 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) >= 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) > 0 && first.CompareTo(first) >= 0)
