5987f668f55549bfaf94f83edcbe772be33418a4
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-12 21:55:24 +0530

    Find Maximum Of 3 String Values
    Find Maximum Of 3 String Values

--- MaxOf3String.cs
+++ MaxOf3String.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace FindMaxOf3String
 {
@@ -6,18 +6,21 @@ namespace FindMaxOf3String
     {
         public static String findmaxstring(String first, String second, String third)
         {
+            //If first-second>0 && first-third>0
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
             {
                 return first;
             }
+            //If second-first>0 && second-third>0
             if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
             {
                 return second;
             }
+            //If third-first>0 && third-second>0
             if (third.CompareTo(second) > 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) >= 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) > 0 && first.CompareTo(first) >= 0)
